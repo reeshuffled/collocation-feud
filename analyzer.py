7b25d097aa17977375e21105ad13ae9932b6bab3
Rees Draminski <rees.draminski@gmail.com>
2022-02-20 17:34:05 -0500

    Initial commmit

---
+++ analyzer.py
@@ -0,0 +1,30 @@
+# every tenth word 1-60,000
+# ID	lemma	lemPoS	coll	collPoS	MI	freq	[% coll < node]
+
+import json
+
+data = {}
+with open("collocations.txt", "r") as corpus:
+    lines = corpus.readlines()
+
+    current_word = {
+        "word": None,
+        "colocations": []
+    }
+
+    for line in lines:
+        # seperate the data with TAB as the delimiter
+        cols = line.split("\t")
+
+        curr = cols[1]
+
+        if curr not in data:
+            data[curr] = []
+
+        data[curr].append({
+            "assoc": cols[3],
+             "freq": cols[6]
+        })
+
+with open("data.json", "w") as file:
+    file.write(json.dumps(data))
---
+++ js/app.js
@@ -0,0 +1,73 @@
+// DOM element references
+const wordEl = document.getElementById("word");
+const inputEl = document.getElementById("guessInput");
+const boardEl = document.getElementById("board");
+const newWordBtn = document.getElementById("newWord");
+const scoreEl = document.getElementById("score");
+
+/**
+ * Initalize the UI components of the game.
+ */
+(async function initUI() {
+    // load the collocation data
+    const response = await fetch("data.json");
+    const data = await response.json();
+
+    // select a random word when the data loads
+    selectRandomWord(data);
+
+    newWordBtn.onclick = () => selectRandomWord(data);
+})();
+
+/**
+ * Select a random word from the dataset to show.
+ * @param {Object[]} data 
+ */
+function selectRandomWord(data) {
+    const key = Object.keys(data)[Math.floor(Math.random() * Object.keys(data).length)];
+    
+    wordEl.innerText = key;
+
+    inputEl.onkeydown = e => {
+        if (e.key == "Enter")
+        {
+            guessWord(data[key]);
+        }
+    }
+}
+
+/**
+ * 
+ * @param {Object[]} data 
+ */
+function guessWord(data) {
+    // get the user's guess and find it was in the collocation corpus
+    const guess = inputEl.value.toLowerCase();
+    const hit = data.find(x => x.assoc == guess);
+
+    // check if the table is full or not
+    const nextEmptyCell = [...board.querySelectorAll("td")].find(x => x.innerText == "");
+    if (nextEmptyCell)
+    {
+        if (hit)
+        {
+            // show the guess and the collocation frequency
+            nextEmptyCell.innerHTML = `
+                ${guess} <span style="float: right">${hit.freq}</span>
+            `;
+
+            // increase the score display
+            scoreEl.innerText = (parseFloat(scoreEl.innerText) + parseFloat(hit.freq)).toFixed(2);
+        }
+        else
+        {
+            // show the guess with an X next to it to show the user that the guess was incorrect
+            nextEmptyCell.innerHTML = `
+                ${guess} <span style="float: right">X</span>
+            `;
+        }
+    }
+
+    // clear the guess input
+    inputEl.value = "";
+}
